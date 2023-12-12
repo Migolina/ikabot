c3b9940a0672052d05c14c4287f3f2e5de70ccf9
Migolina <taylandurmaz@windowslive.com>
2023-12-12 13:12:53 +0300

    barbarian lvl1 to lvl30 trying to not get ip banned

--- ikabot/helpers/random_wait.py
+++ ikabot/helpers/random_wait.py
@@ -3,6 +3,5 @@ import time
 
 def wait_random_minutes():
     """ Wait for a random amount of time between 1 and 3 minutes. """
-    minutes = random.randint(2, 5)  # Randomly choose between 1 and 3 minutes
-    print(f"Waiting for {minutes} minute(s)...")
-    time.sleep(minutes * 60)  # Convert minutes to seconds and wait
+    minutes = random.randint(45, 80)
+    time.sleep(minutes * 60)
